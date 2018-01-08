466104e21c712ba06f04202a26716c6882d17ead
glen newton <glen.newton@gmail.com>
2018-01-08 01:04:12 -0500

    Some adjustments for koroc

--- dbStructs.go
+++ dbStructs.go
@@ -5,39 +5,57 @@ import (
 )
 
 type Article struct {
-	Abstract        string
-	Authors         []Author    `gorm:"many2many:article_author;"`
-	Chemicals       []*Chemical `gorm:"many2many:article_chemical;"`
-	Citations       []*Citation `gorm:"many2many:article_citation;"`
-	Day             int
-	Genes           []Gene `gorm:"many2many:article_gene;"`
-	ID              int64  `gorm:"primary_key"` // PMID
-	Issue           string `sql:"size:32"`
-	Journal         *Journal
-	JournalID       sql.NullInt64
-	Keywords        []*Keyword `gorm:"many2many:article_keyword;"`
-	KeywordsOwner   string     `sql:"size:128"`
-	Language        string     `sql:"size:3"`
-	MeshDescriptors []*MeshDescriptor
-	Month           string `sql:"size:8"`
-	OtherId         []OtherID
-	Title           string
-	Version         int `sql:"size:2"`
-	Volume          string
-	Year            int   `sql:"size:4"`
-	DateRevised     int64 //YYYYMMDD 20140216
-	DataBanks       []*DataBank
+	Abstract         string
+	Authors          []Author    `gorm:"many2many:article_author;"`
+	Chemicals        []*Chemical `gorm:"many2many:article_chemical;"`
+	Citations        []*Citation `gorm:"many2many:article_citation;"`
+	Day              int
+	Genes            []Gene `gorm:"many2many:article_gene;"`
+	ID               int64  `gorm:"primary_key"` // PMID
+	Issue            string `sql:"size:32"`
+	Journal          *Journal
+	JournalID        sql.NullInt64
+	Keywords         []*Keyword `gorm:"many2many:article_keyword;"`
+	KeywordsOwner    string     `sql:"size:128"`
+	Language         string     `sql:"size:3"`
+	MeshDescriptors  []*MeshDescriptor
+	Month            string `sql:"size:8"`
+	OtherId          []OtherID
+	Title            string
+	Version          int `sql:"size:2"`
+	Volume           string
+	Year             int   `sql:"size:4"`
+	DateRevised      int64 //YYYYMMDD 20140216
+	DataBanks        []*DataBank
+	ArticleIDs       []*ArticleID
+	Retracted        bool
+	PublicationTypes []*PublicationType `gorm:"many2many:article_publicationtype;"`
+}
+
+type PublicationType struct {
+	ID   int64  `gorm:"primary_key"`
+	UI   string `sql:"size:8"`
+	Name string `sql:"size:64"`
+}
+
+type ArticleID struct {
+	OtherArticleID string `sql:"size:64"`
+	Type           string `sql:"size:12"`
+	ID             int64  `gorm:"primary_key"`
+	ArticleID      int64
 }
 
 type DataBank struct {
-	ID               int `gorm:"primary_key"`
-	Name             string
+	ID               int64  `gorm:"primary_key"`
+	Name             string `sql:"size:32"`
 	AccessionNumbers []*AccessionNumber
+	ArticleID        int64
 }
 
 type AccessionNumber struct {
-	ID     int `gorm:"primary_key"`
-	Number string
+	ID         int64  `gorm:"primary_key"`
+	Number     string `sql:"size:32"`
+	DataBankID int64
 }
 
 type OtherID struct {
@@ -55,11 +73,12 @@ type Journal struct {
 }
 
 type Author struct {
-	ID          int    `gorm:"primary_key"`
-	LastName    string `sql:"size:48"`
-	FirstName   string `sql:"size:16"`
-	MiddleName  string `sql:"size:16"`
-	Affiliation string
+	ID             int    `gorm:"primary_key"`
+	LastName       string `sql:"size:48"`
+	FirstName      string `sql:"size:16"`
+	MiddleName     string `sql:"size:16"`
+	Affiliation    string
+	CollectiveName string
 }
 
 type Keyword struct {
