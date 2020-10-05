cca9712c98838211fd125492b7976e6f31a13122
Glen Newton <glen.newton@gmail.com>
2020-10-05 07:57:21 -0400

    Ints all properly sized

--- dbStructs.go
+++ dbStructs.go
@@ -1,7 +1,7 @@
 package pubmedSqlStructs
 
 import (
-	"database/sql"
+//	"database/sql"
 )
 
 type Article struct {
@@ -11,12 +11,12 @@ type Article struct {
 	Chemicals        []*Chemical `gorm:"many2many:article_chemical;"`
 	Citations        []*Citation `gorm:"many2many:article_citation;"`
 	DataBanks        []*DataBank
-	DateRevised      int64 //YYYYMMDD 20140216
-	Day              int
-	ID               int32  `gorm:"primary_key"` // PMID
-	Issue            string `sql:"size:32"`
+	DateRevised      uint64 //YYYYMMDD 20140216
+	Day              uint8  `sql:"type:int1"`
+	ID               uint32 `gorm:"primary_key" sql:"type:int4"` // PMID
+	Issue            string `sql:"size:32" sql:"type:int4"`
 	Journal          *Journal
-	JournalID        sql.NullInt64
+	JournalID        uint32     `sql:"type:int4"`
 	Keywords         []*Keyword `gorm:"many2many:article_keyword;"`
 	KeywordsOwner    string     `sql:"size:128"`
 	Language         string     `sql:"size:3"`
@@ -26,13 +26,13 @@ type Article struct {
 	PublicationTypes []*PublicationType `gorm:"many2many:article_publicationtype;"`
 	Retracted        bool
 	Title            string
-	Version          int `sql:"size:2"`
+	Version          uint8 `sql:"type:int1"`
 	Volume           string
-	Year             int `sql:"size:4"`
+	Year             uint16 `sql:"type:int2"`
 }
 
 type PublicationType struct {
-	ID   int64  `gorm:"primary_key"`
+	ID   uint32 `gorm:"primary_key" sql:"type:int4"`
 	UI   string `sql:"size:8"`
 	Name string `sql:"size:64"`
 }
@@ -40,31 +40,31 @@ type PublicationType struct {
 type ArticleID struct {
 	OtherArticleID string `sql:"size:64"`
 	Type           string `sql:"size:12"`
-	ID             int32  `gorm:"primary_key"`
-	ArticleID      int32
+	ID             uint32 `gorm:"primary_key" sql:"type:int4"`
+	ArticleID      uint32 `sql:"type:int4"`
 }
 
 type DataBank struct {
-	ID               int32  `gorm:"primary_key"`
+	ID               uint32 `gorm:"primary_key" sql:"type:int4"`
 	Name             string `sql:"size:32"`
 	AccessionNumbers []*AccessionNumber
-	ArticleID        int32
+	ArticleID        uint32 `sql:"type:int4"`
 }
 
 type AccessionNumber struct {
-	ID         int32  `gorm:"primary_key"`
+	ID         uint32 `gorm:"primary_key" sql:"type:int4"`
 	Number     string `sql:"size:32"`
-	DataBankID int32
+	DataBankID uint32 `sql:"type:int2"`
 }
 
 type OtherID struct {
-	ID      int `gorm:"primary_key"`
+	ID      uint32 `gorm:"primary_key" sql:"type:int4"`
 	Source  string
 	OtherID string
 }
 
 type Journal struct {
-	ID              int `gorm:"primary_key"`
+	ID              uint32 `gorm:"primary_key" sql:"type:int4"`
 	Articles        []Article
 	IsoAbbreviation string `sql:"size:128"`
 	Issn            string `sql:"size:10"`
@@ -72,7 +72,7 @@ type Journal struct {
 }
 
 type Author struct {
-	ID             int    `gorm:"primary_key"`
+	ID             uint32 `gorm:"primary_key" sql:"type:int4"`
 	LastName       string `sql:"size:48"`
 	FirstName      string `sql:"size:16"`
 	MiddleName     string `sql:"size:16"`
@@ -81,40 +81,40 @@ type Author struct {
 }
 
 type Keyword struct {
-	ID         int `gorm:"primary_key"`
+	ID         uint32 `gorm:"primary_key" sql:"type:int4"`
 	MajorTopic bool
 	Name       string `sql:"size:128"`
 }
 
 type MeshDescriptor struct {
-	ID         int `gorm:"primary_key"`
+	ID         uint32 `gorm:"primary_key" sql:"type:int4"`
 	Name       string
 	Type       string `sql:"size:32"`
 	MajorTopic bool
 	Qualifiers []*MeshQualifier
-	ArticleID  int
+	ArticleID  uint32 `sql:"type:int4"`
 	UI         string
 }
 
 type MeshQualifier struct {
-	ID               int `gorm:"primary_key"`
+	ID               uint32 `gorm:"primary_key" sql:"type:int4"`
 	MajorTopic       bool
 	Name             string `sql:"size:128"`
-	MeshDescriptorID int
+	MeshDescriptorID uint32 `sql:"type:int4"`
 	UI               string
 }
 
 type Gene struct {
-	ID   int `gorm:"primary_key"`
+	ID   uint32 `gorm:"primary_key" sql:"type:int4"`
 	Name string
 }
 
 type Chemical struct {
-	ID       int `gorm:"primary_key"`
+	ID       uint32 `gorm:"primary_key" sql:"type:int4"`
 	Name     string
 	Registry string `sql:"size:32"`
 }
 
 type Citation struct {
-	ID int `gorm:"primary_key"`
+	ID uint32 `gorm:"primary_key" sql:"type:int4"`
 }
